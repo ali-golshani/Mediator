dcc4691f2bda9da6bda499ec99eb0cc4f09ed77e
golshani.a <golshani.a@ime.co.ir>
2025-02-25 15:50:08 +0330

    MediatorException

--- Mediator/Exceptions/MediatorException.cs
+++ Mediator/Exceptions/MediatorException.cs
@@ -2,7 +2,5 @@
 
 public abstract class MediatorException : Exception
 {
-    private protected MediatorException() { }
     private protected MediatorException(string? message) : base(message) { }
-    private protected MediatorException(string? message, Exception? innerException) : base(message, innerException) { }
 }
