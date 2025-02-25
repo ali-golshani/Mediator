648100bb44745d0f4607f4e1f89678f2d2d01c77
golshani.a <golshani.a@ime.co.ir>
2025-02-25 16:33:28 +0330

    Mediator.Extensions

--- Mediator/Extensions/Extensions.cs
+++ Mediator/Extensions/Extensions.cs
@@ -1,9 +1,20 @@
 ﻿using Mediator.Exceptions;
+using Mediator.Middlewares;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mediator.Extensions;
 
 public static class Extensions
 {
+    public static void RegisterMiddlewares<TPipelineConfiguration>(this IServiceCollection services)
+        where TPipelineConfiguration : IKeyedPipelineConfiguration
+    {
+        foreach (var type in TPipelineConfiguration.Middlewares())
+        {
+            services.AddKeyedTransient(typeof(IMiddleware<,>), TPipelineConfiguration.PipelineName, type);
+        }
+    }
+
     public static TRequest AsRequestType<TRequest, TResponse>(this IRequest<TRequest, TResponse> request)
     {
         if (request is not TRequest result)
