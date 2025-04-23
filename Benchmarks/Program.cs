bd2a7eeee386f5ac07d22ae85a9707774022693f
Ali Golshani <ali.golshani@hotmail.com>
2025-04-23 19:03:12 +0330

    Benchmarks

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -6,7 +6,30 @@ internal static class Program
 {
     static void Main()
     {
-        BenchmarkRunner.Run<SendBenchmarks>();
+        Console.WriteLine("Select Benchmark:");
+        Console.WriteLine("1. Send Benchmark");
+        Console.WriteLine("2. Publish Benchmark");
+        Console.Write("Select: ");
+
+        var key = Console.ReadKey();
+        Console.WriteLine();
+
+        if (key.Key == ConsoleKey.D1 || key.Key == ConsoleKey.NumPad1)
+        {
+            BenchmarkRunner.Run<SendBenchmarks>();
+        }
+        if (key.Key == ConsoleKey.D2 || key.Key == ConsoleKey.NumPad2)
+        {
+            BenchmarkRunner.Run<PublishBenchmarks>();
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Invalid Input");
+        }
+
+        Console.WriteLine();
+        Console.Write("Press Enter to Exit .");
         Console.ReadLine();
     }
 }
