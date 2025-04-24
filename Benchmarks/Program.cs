621c38a83a06053f5424aa09477c2254ebae6ec8
Ali Golshani <ali.golshani@hotmail.com>
2025-04-25 02:04:58 +0330

    Benchmarks . BenchmarkApp

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,38 +1,11 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace Benchmarks;
+﻿namespace Benchmarks;
 
 internal static class Program
 {
     static void Main()
     {
-        Benchmark();
-        Exit();
-    }
+        BenchmarkApp.Run();
 
-    private static void Benchmark()
-    {
-        Console.WriteLine("Select Benchmark:");
-        Console.WriteLine("1. Send");
-        Console.WriteLine("2. Publish");
-        Console.WriteLine();
-        Console.Write("Select (1|2): ");
-
-        var key = Console.ReadKey();
-        Console.WriteLine();
-
-        if (key.Key == ConsoleKey.D1 || key.Key == ConsoleKey.NumPad1)
-        {
-            BenchmarkRunner.Run<SendBenchmarks>();
-        }
-        else if (key.Key == ConsoleKey.D2 || key.Key == ConsoleKey.NumPad2)
-        {
-            BenchmarkRunner.Run<PublishBenchmarks>();
-        }
-    }
-
-    private static void Exit()
-    {
         Console.WriteLine();
         Console.Write("Press Enter to Exit .");
         Console.ReadLine();
