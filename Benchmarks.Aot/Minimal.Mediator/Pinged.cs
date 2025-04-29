81ab45744ea50c1f721dd3578dc04f5138752c1d
Ali Golshani <ali.golshani@hotmail.com>
2025-04-29 14:44:54 +0330

    delete project Benchmarks.Aot

--- Benchmarks.Aot/Minimal.Mediator/Pinged.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Minimal.Mediator;
-
-internal class Pinged : INotification { }
-
-internal class PingedHandler : INotificationHandler<Pinged>
-{
-    public Task Handle(Pinged notification, CancellationToken cancellationToken) => Task.CompletedTask;
-}
